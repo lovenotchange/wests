216e9a02bd2adf5fe0ef57815283cccc8b07d558
2524670963@qq.com <2524670963@qq.com>
2020-06-11 22:38:37 +0800

    全部提交

---
+++ src/main/java/com/example/west/WestApplication.java
@@ -0,0 +1,13 @@
+package com.example.west;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/west/WestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.west;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
